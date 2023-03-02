fa867fdafcbf41ec26e8e6b9efaba971bcec4caf
Sunile Manjee <sunilemanjee@gmail.com>
2023-03-02 10:03:08 -0600

    ini v
    ini v

---
+++ getagent.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Download the Elastic APM agent
+wget "https://<YourAzureContainer>.blob.core.windows.net/public-apm-agent/elastic-apm-agent-1.36.0.jar" -O "/databricks/jars/elastic-apm-agent.jar"
+
+
